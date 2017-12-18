5149e43a6fa36be62a37296d204a6d04e0bd5ad6
Abraham Tsang <wai@dr.com>
2017-12-17 19:48:08 -0500

    now proclaims new feature!

--- small.java
+++ small.java
@@ -3,7 +3,7 @@ public class small {
   public static void main(String args[]) {
     
     System.out.println("I am a small program.");
-    
+    System.out.println("This new feature is in branch 1, whoopee!");
     
   }
   
