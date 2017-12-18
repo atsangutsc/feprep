7e21a89efca4f00216b64b7b6c2b9c768eae0e4e
Abraham Tsang <wai@dr.com>
2017-12-17 19:51:14 -0500

    now shows b2 for reals!

--- small.java
+++ small.java
@@ -2,7 +2,7 @@ public class small {
   
   public static void main(String args[]) {
     
-    System.out.println("I am a small program.");
+    System.out.println("I am a small program.  This is branch TWO FOR REALSIES THIS TIME!");
     
     
   }
